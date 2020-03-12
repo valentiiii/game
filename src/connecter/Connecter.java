9734cf8d87dc2a0d5166cef1ac1a1a1ecd05dabd
valentiiii <yuumapage2@gmail.com>
2020-03-13 01:34:51 +0900

    あ

--- src/connecter/Connecter.java
+++ src/connecter/Connecter.java
@@ -1,5 +1,50 @@
 package connecter;
 
+import static constant.GrConst.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/* データベースに接続と切断を行うクラス*/
 public class Connecter {
 
-}
+    /* シングルトンパターンでインスタンスを作成*/
+    private final static Connecter qConnecter = new Connecter();
+
+    /* コンストラクタを作成*/
+    private Connecter() {
+    }
+
+    /* データベースに接続するメソッド
+     *
+     * @return con
+     */
+    public Connection getConnection() {
+
+        Connection con = null;
+
+        try {
+
+            Class.forName(DRIVER_NAME);
+            con = DriverManager.getConnection(DRIVER_URL, USER_NAME, PASSWORD);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+
+    }
+
+    /**
+     * データベースから切断するメソッド
+     *
+     * @param Connection
+     *            con 接続オブジェクト
+     * @throws SQLException
+     */
+    public void closeConnection(Connection con) {
+
+        try {
+
+            con.close();
+        }
